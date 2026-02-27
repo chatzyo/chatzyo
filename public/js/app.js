4dc41357fc3608b302ea79500616c2418f8d4812
chatzyo <gaze.qivions@gmail.com>
2026-02-27 20:10:45 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
+const startBtn = document.getElementById("startBtn");
+const localVideo = document.getElementById("localVideo");
 
+startBtn.addEventListener("click", async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    });
+
+    localVideo.srcObject = stream;
+  } catch (error) {
+    console.error("Error accessing camera:", error);
+    alert("Unable to access camera or microphone.");
+  }
+});
